7defa69a18411d247f4772b682e61e148efbb50c
RockEt <82693254+Rocket-007@users.noreply.github.com>
2022-09-06 19:27:28 +0100

    this is the overall fix, but i still dont how

--- GameWorld.gd
+++ GameWorld.gd
@@ -6,23 +6,19 @@ class_name GameWorld
 signal started
 signal finished
 
-enum Cell{OBSTACLE, GROUND, OUTER}
+enum Cell {OBSTACLE, GROUND, OUTER}
 
 export var inner_size := Vector2(10, 8)
-export var parimeter_size := Vector2(1,1)
-export (float, 0, 1) var ground_probability := 0.1
+export var perimeter_size := Vector2(1, 1)
+export(float, 0 , 1) var ground_probability := 0.1
 
 # Public variables
-var size := inner_size + 2 * parimeter_size
+onready var size := inner_size + 2 * perimeter_size
 
 # Private variables
 onready var _tile_map : TileMap = $TileMap
 var _rng := RandomNumberGenerator.new()
 
-func _ready() -> void:
-	_rng.randomize()
-	setup()
-	generate()
 
 func setup() -> void:
 	# Sets the game window size to twice the resolution of the world.
@@ -31,13 +27,12 @@ func setup() -> void:
 	OS.set_window_size(2 * map_size_px)
 
 
-
 func generate() -> void:
 	# Although there's no other nodes to use these signals, we're including them
 	# to show when and how to emit them.
 	# Watch our signals tutorial for more information.
 	emit_signal("started")
-	generate_inner()
+	generate_perimeter()
 	generate_inner()
 	emit_signal("finished")
 
@@ -45,25 +40,35 @@ func generate() -> void:
 func generate_perimeter() -> void:
 	# Fills the outer edges of the map with the border tiles.
 	# Randomly selects from the tiles marked as `Cell.OUTER` using the funciton `_pick_random_texture`.
+	# The two nested loops below fill the outer columns and outer row
+	# of the map, respectively.
 	for x in [0, size.x - 1]:
 		for y in range(0, size.y):
 			_tile_map.set_cell(x, y, _pick_random_texture(Cell.OUTER))
 	for x in range(1, size.x - 1):
 		for y in [0, size.y - 1]:
 			_tile_map.set_cell(x, y, _pick_random_texture(Cell.OUTER))
-		
 
 
 func generate_inner() -> void:
 	# Fills the inside of the map the inner tiles from the remaining types: `Cell.GROUND` and `Cell.OBSTACLE` using the
 	# `get_random_tile` function that takes the probability for `Cell.GROUND` tiles to have some more control
 	# over what types of tiles we'll be placing.
-	return
+	for x in range(1, size.x - 1):
+		for y in range(1, size.y - 1):
+			var cell := get_random_tile(ground_probability)
+			_tile_map.set_cell(x, y, cell)
+
+
+func get_random_tile(probability: float) -> int:
+	# Randomly picks a tile id between `Cell.GROUND` and `Cell.OBSTACLE` types given the ground probability.
+	# Returns the id of the cell in the TileSet resource.
+	return _pick_random_texture(Cell.GROUND) if _rng.randf() < probability else _pick_random_texture(Cell.OBSTACLE)
+
 
 func _pick_random_texture(cell_type: int) -> int:
-	#randomly picks a tile based on the three types: cell.outer, cell.ground cell.obstacle
-	#returns the id og the cell in the tileset resource
-	
+	# Randomly picks a tile based on the three types: `Cell.OUTER`, `Cell.GROUND` & `Cell.OBSTACLE`.
+	# Returns the id of the cell in the TileSet resource.
 	var interval := Vector2()
 	if cell_type == Cell.OUTER:
 		interval = Vector2(0, 9)
@@ -72,5 +77,14 @@ func _pick_random_texture(cell_type: int) -> int:
 	elif cell_type == Cell.OBSTACLE:
 		interval = Vector2(15, 27)
 	return _rng.randi_range(interval.x, interval.y)
-	
-		
+
+
+func _ready() -> void:
+	_rng.randomize()
+	setup()
+	generate()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("click") or event.is_action_pressed("ui_accept"):
+		generate()
